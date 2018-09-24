f7434040060fa6f187a2439f6062e4da8a7e53e9
Ivaylo Botusharov <ivaylo.botusharov@gmail.com>
2018-09-25 00:30:05 +0300

    Added Start.bat file for restoring packages, building and starting both apps

---
+++ Start.bat
@@ -0,0 +1,15 @@
+echo "Starting Build"
+dotnet build Northwind.RestApis\Northwind.RestApis.sln
+dotnet build Northwind.WebApps\Northwind.WebApps.sln
+
+echo "Starting Publish"
+dotnet publish Northwind.RestApis\Northwind.Api\Northwind.Api.csproj -c Release
+dotnet publish Northwind.WebApps\Northwind.Mvc\Northwind.Mvc.csproj -c Release
+    
+pushd Northwind.RestApis\Northwind.Api\bin\Release\netcoreapp2.1\publish\
+start "Northwind Rest API" cmd /c "dotnet Northwind.Api.dll --server.urls ""https://localhost:44377""
+popd
+
+pushd Northwind.WebApps\Northwind.Mvc\bin\Release\netcoreapp2.1\publish\
+start "Northwind Web App (MVC)" cmd /c "dotnet Northwind.Mvc.dll --server.urls ""https://localhost:44364""
+popd
