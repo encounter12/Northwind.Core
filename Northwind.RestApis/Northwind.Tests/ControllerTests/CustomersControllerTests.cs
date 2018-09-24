6c11a5e16a41d7062234bd7d183355f337c882bb
Ivaylo Botusharov <ivaylo.botusharov@gmail.com>
2018-09-24 19:58:46 +0300

    Northwind.RestApis - added unit tests for CustomersController

--- Northwind.RestApis/Northwind.Tests/ControllerTests/CustomersControllerTests.cs
+++ Northwind.RestApis/Northwind.Tests/ControllerTests/CustomersControllerTests.cs
@@ -1,14 +1,71 @@
-using System;
+using FizzWare.NBuilder;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Northwind.Api.Controllers;
+using Northwind.Services.Contracts;
+using Northwind.Services.Models;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace Northwind.Tests.ControllerTests
 {
     public class CustomersControllerTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly CustomersController customersController;
+        private Mock<ICustomerService> mockedCustomerService;
+        private Mock<IOrderService>  mockedOrderService;
 
+        public CustomersControllerTests()
+        {
+            mockedCustomerService = new Mock<ICustomerService>();
+            mockedOrderService = new Mock<IOrderService>();
+            customersController = new CustomersController(
+                mockedCustomerService.Object, mockedOrderService.Object);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(4)]
+        [InlineData(6)]
+        public void GetCustomers_Should_ReturnOkResult_When_NoError_AndCustomersExist(int customersCount)
+        {
+            // Arrange
+            this.mockedCustomerService.Setup(customerService => customerService.GetCustomers())
+                .Returns(this.CreateCustomersList(customersCount).AsQueryable());
+
+            // Act
+            var okResult = customersController.GetCustomers();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult.Result);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(3)]
+        [InlineData(5)]
+        public void GetCustomers_Should_ReturnAllCustomers_When_CustomersExist(int customersCount)
+        {
+            // Arrange
+            this.mockedCustomerService.Setup(customerService => customerService.GetCustomers())
+                .Returns(this.CreateCustomersList(customersCount).AsQueryable());
+
+            // Act
+            var okResult = customersController.GetCustomers().Result as OkObjectResult;
+
+            // Assert
+            var customers = Assert.IsAssignableFrom<IQueryable<CustomerListViewModel>>(okResult.Value);
+            Assert.Equal(customersCount, customers.Count());
+        }
+
+        private IList<CustomerListViewModel> CreateCustomersList(int customersCount)
+        {
+            IList<CustomerListViewModel> customers = Builder<CustomerListViewModel>
+                .CreateListOfSize(customersCount)
+                .Build();
+
+            return customers;
         }
     }
 }
