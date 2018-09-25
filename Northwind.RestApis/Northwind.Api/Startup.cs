4081b00059d09d57a190692e539d429be780c36d
Ivaylo Botusharov <ivaylo.botusharov@gmail.com>
2018-09-25 03:01:17 +0300

    added Swagger documentation to Northwind Rest API Project

--- Northwind.RestApis/Northwind.Api/Startup.cs
+++ Northwind.RestApis/Northwind.Api/Startup.cs
@@ -10,6 +10,10 @@ using Northwind.Data.Repositories.Contracts;
 using Northwind.Data.UnitOfWork;
 using Northwind.Services;
 using Northwind.Services.Contracts;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace Northwind.Api
 {
@@ -26,6 +30,29 @@ namespace Northwind.Api
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Northwind REST API",
+                    Description = "A simple API for working with Northwind database",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Ivaylo Botusharov",
+                        Email = string.Empty,
+                        Url = "https://twitter.com/ivo.botusharov"
+                    },
+                    License = new License
+                    {
+                        Name = "MIT License",
+                        Url = "https://example.com/license"
+                    }
+                });
+            });
+
             var connection = @"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=True;ConnectRetryCount=0";
             services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(connection));
 
@@ -41,6 +68,17 @@ namespace Northwind.Api
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.DocumentTitle = "Northwind REST API";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Northwind REST API");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
