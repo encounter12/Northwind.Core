18a543c66b76b56b990c98baf28b9821555da117
Ivaylo Botusharov <ivaylo.botusharov@gmail.com>
2018-11-20 22:33:36 +0200

    removed OnConfiguring() method in MasterContext

--- Northwind.RestApis/Northwind.Data/MasterContext.cs
+++ Northwind.RestApis/Northwind.Data/MasterContext.cs
@@ -1,32 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Northwind.Infrastructure.Models;
 
 namespace Northwind.Data
 {
     public class MasterContext : DbContext
     {
-        private readonly IConfiguration configuration;
-
         public MasterContext()
         {
         }
 
-        public MasterContext(DbContextOptions<MasterContext> options, IConfiguration configuration)
+        public MasterContext(DbContextOptions<MasterContext> options)
             : base(options)
-        {
-            this.configuration = configuration;
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            AppData appData = this.configuration.GetSection("AppData").Get<AppData>();
-
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder
-                    .UseSqlServer(appData.MasterDbConnectionString);
-            }
+        {    
         }
     }
 }
